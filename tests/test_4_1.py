febad20ebf63c3be3562913be1ca24167b233037
denis <denis.i.myshkov@rt.ru>
2020-08-07 15:21:24 +0300

    test_4_1 was rewrited

--- tests/test_4_1.py
+++ tests/test_4_1.py
@@ -3,12 +3,12 @@ from selenium import webdriver
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import NoSuchElementException
-
+import time
 
 @pytest.fixture
 def driver(request):
     wd = webdriver.Chrome()
-    wd.implicitly_wait(10)
+    wd.implicitly_wait(15)
     request.addfinalizer(wd.quit)
     return wd
 
@@ -22,26 +22,21 @@ def test_for_lection_4_task_1(driver):
 
     x = 0
 
-    while x < menu_items_len:
+    if x < menu_items_len:
         menu_items = driver.find_elements_by_class_name("name")
         menu_items[x].click()
-        try:
-            Element = driver.find_element_by_tag_name('h1')
-        except NoSuchElementException:
-            return False
+        element = driver.find_elements_by_tag_name("h1")
+        assert len(element) == 1
         new_menu_items_len = len(driver.find_elements_by_class_name("name"))
         y = new_menu_items_len - menu_items_len
         c = 1
-        while y > 0:
+        if y > 0:
             menu_items = driver.find_elements_by_class_name("name")
             menu_items[x+c].click()
-            try:
-                Element = driver.find_element_by_tag_name('h1')
-            except NoSuchElementException:
-                return False
+            element = driver.find_elements_by_tag_name("h1")
+            assert len(element) == 1
             c+=1
             y-=1
+        driver.find_element_by_class_name("logotype").click()
         x+=1
-
-    WebDriverWait(driver, 10).until(EC.title_is("vQmods | My Store"))
 
