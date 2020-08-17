4da7a8de665bcfcd67ba488b210e4fcc2ab897ad
denis <denis.i.myshkov@rt.ru>
2020-08-17 21:09:14 +0300

    test for lection 5 task 2 was refactored

--- tests/test_5_2.py
+++ tests/test_5_2.py
@@ -17,46 +17,40 @@ def test_for_lection_5_task_2(driver):
     product = driver.find_elements_by_xpath("/html/body/div[2]/div/div[2]/div/div[2]/div[2]/div[4]/div/ul/li")[0]
 
     product_name = product.find_element_by_xpath("./a[1]").get_attribute("title")
-    
     price_normal = product.find_element_by_xpath("./a[1]/div[4]/s").get_attribute("innerText")
     price_normal_text_decoration = product.find_element_by_xpath("./a[1]/div[4]/s").value_of_css_property("text-decoration-line")
     price_normal_color = product.find_element_by_xpath("./a[1]/div[4]/s").value_of_css_property("color")
     a = int(price_normal_color[5:8])
     b = int(price_normal_color[10:13])
     c = int(price_normal_color[15:18])
-
     price_normal_font_size = float(product.find_element_by_xpath("./a[1]/div[4]/s").value_of_css_property("font-size")[0:-2])
-
     price_special = product.find_element_by_xpath("./a[1]/div[4]/strong").get_attribute("innerText")
     price_special_font_weight = product.find_element_by_xpath("./a[1]/div[4]/strong").value_of_css_property("font-weight")
     price_special_color = product.find_element_by_xpath("./a[1]/div[4]/strong").value_of_css_property("color")
-
     price_special_font_size = float(product.find_element_by_xpath("./a[1]/div[4]/strong").value_of_css_property("font-size")[0:-2])
 
-    driver.find_element_by_xpath("/html/body/div[2]/div/div[2]/div/div[2]/div[2]/div[4]/div/ul/li/a[1]/div[1]/img").click()
+    product.click()
 
-    product_page_name = driver.find_element_by_xpath("/html/body/div[2]/div/div[2]/div/div[2]/div[2]/div[2]/div[1]/h1").get_attribute("innerText")
-    
-    product_page_price_normal = driver.find_element_by_xpath("/html/body/div[2]/div/div[2]/div/div[2]/div[2]/div[2]/div[2]/div[2]/div[2]/s").get_attribute("innerText")
-    product_page_price_normal_text_decoration = driver.find_element_by_xpath("/html/body/div[2]/div/div[2]/div/div[2]/div[2]/div[2]/div[2]/div[2]/div[2]/s").value_of_css_property("text-decoration-line")
-    product_page_price_normal_color = driver.find_element_by_xpath("/html/body/div[2]/div/div[2]/div/div[2]/div[2]/div[2]/div[2]/div[2]/div[2]/s").value_of_css_property("color")
+    pp_name = driver.find_element_by_tag_name("h1").get_attribute("innerText")
+    pp_price_normal = driver.find_element_by_class_name("regular-price").get_attribute("innerText")
+    pp_price_normal_text_decoration = driver.find_element_by_class_name("regular-price").value_of_css_property("text-decoration-line")
+    pp_price_normal_color = driver.find_element_by_class_name("regular-price").value_of_css_property("color")
+    pp_price_special = driver.find_element_by_class_name("campaign-price").get_attribute("innerText")
+    pp_price_special_font_weight = driver.find_element_by_class_name("campaign-price").value_of_css_property("font-weight")
+    pp_price_special_color = driver.find_element_by_class_name("campaign-price").value_of_css_property("color")
+    d = int(pp_price_normal_color[5:8])
+    e = int(pp_price_normal_color[10:13])
+    f = int(pp_price_normal_color[15:18])
+    pp_price_normal_font_size = float(driver.find_element_by_class_name("regular-price").value_of_css_property("font-size")[0:-2])
+    pp_price_special_font_size = float(driver.find_element_by_class_name("campaign-price").value_of_css_property("font-size")[0:-2])
 
-    product_page_price_special = driver.find_element_by_xpath("/html/body/div[2]/div/div[2]/div/div[2]/div[2]/div[2]/div[2]/div[2]/div[2]/strong").get_attribute("innerText")
-    product_page_price_special_font_weight = driver.find_element_by_xpath("/html/body/div[2]/div/div[2]/div/div[2]/div[2]/div[2]/div[2]/div[2]/div[2]/strong").value_of_css_property("font-weight")
-    product_page_price_special_color = driver.find_element_by_xpath("/html/body/div[2]/div/div[2]/div/div[2]/div[2]/div[2]/div[2]/div[2]/div[2]/strong").value_of_css_property("color")
-    d = int(product_page_price_normal_color[5:8])
-    e = int(product_page_price_normal_color[10:13])
-    f = int(product_page_price_normal_color[15:18])
-
-
-    product_page_price_normal_font_size = float(driver.find_element_by_xpath("/html/body/div[2]/div/div[2]/div/div[2]/div[2]/div[2]/div[2]/div[2]/div[2]/s").value_of_css_property("font-size")[0:-2])
-    product_page_price_special_font_size = float(driver.find_element_by_xpath("/html/body/div[2]/div/div[2]/div/div[2]/div[2]/div[2]/div[2]/div[2]/div[2]/strong").value_of_css_property("font-size")[0:-2])
-    assert product_name == product_page_name
-    assert price_normal == product_page_price_normal
-    assert price_normal_text_decoration == product_page_price_normal_text_decoration
+    assert product_name == pp_name
+    assert price_normal == pp_price_normal
+    assert price_normal_text_decoration == pp_price_normal_text_decoration
     assert a == b == c
     assert price_normal_font_size < price_special_font_size
-    assert price_special == product_page_price_special
-    assert price_special_font_weight == product_page_price_special_font_weight 
+    assert price_special == pp_price_special
+    assert price_special_font_weight == pp_price_special_font_weight 
     assert d == e == f
-    assert product_page_price_normal_font_size < product_page_price_special_font_size
+    assert pp_price_normal_font_size < pp_price_special_font_size
+
