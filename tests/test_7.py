5dad9fe9c8be3d828df1c7ebc2a832da7707c20e
denis <denis.i.myshkov@rt.ru>
2020-08-30 13:47:41 +0300

    waits was added for lection 7

--- tests/test_7.py
+++ tests/test_7.py
@@ -3,6 +3,7 @@ from selenium import webdriver
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.common.by import By
 import time
 
 
@@ -19,14 +20,15 @@ def test_for_lection_7(driver):
         driver.get("https://litecart.stqa.ru/en/")
         driver.find_element_by_xpath("//li[@class='product column shadow hover-light']").click()
         driver.find_element_by_xpath("//button[@name='add_cart_product']").click()
-        time.sleep(1)
-        assert int(driver.find_element_by_class_name("quantity").text) == quantity
-        quantity+=1
+        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, f".//span[contains(@class, 'quantity') and contains(., '{quantity}')]")))
+        quantity+=1 
 
     driver.get("https://litecart.stqa.ru/en/checkout")
     number_of_product = len(driver.find_elements_by_xpath("//td[@class='tax']"))
     while number_of_product != 0:
         driver.get("https://litecart.stqa.ru/en/checkout")
+        element = driver.find_elements_by_xpath(".//td[@class='tax']")[number_of_product-1] 
         driver.find_element_by_name("remove_cart_item").click()
+        EC.staleness_of(element)
         number_of_product-=1
 
